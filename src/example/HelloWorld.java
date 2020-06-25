e72754cb0d1c8bc4fb99343b25662750c85693df
KEJdev <kej12516@gmail.com>
2020-06-25 22:39:32 +0900

    HelloJava

---
+++ src/example/HelloWorld.java
@@ -0,0 +1,16 @@
+package example;
+
+/**
+ * 현재 클래스는 헬로우 월드를 출력하기 위한 클래스입니다.
+ * */
+public class HelloWorld {
+
+    /**
+     * 메인 입니다.
+     * */
+    public static void main(String[] args){
+        System.out.println("Hello World!!!");
+        System.out.println("I LOVE PYTHON ");
+    }
+}
+
