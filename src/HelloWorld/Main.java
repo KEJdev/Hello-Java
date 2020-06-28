28d9d75618be177f6b3e72fc82e95e4a9c56e1c3
KEJdev <kej12516@gmail.com>
2020-06-28 23:10:49 +0900

    자바 테스트

---
+++ src/HelloWorld/Main.java
@@ -0,0 +1,16 @@
+package HelloWorld;
+
+/**
+ * 현재 클래스는 헬로우 월드를 출력하기 위한 클래스입니다.
+ * */
+public class Main {
+
+    /**
+     * 메인 입니다.
+     * */
+
+    public static void main(String[] args){
+        System.out.println("Hello World!!!");
+        System.out.println("I LOVE PYTHON ");
+    }
+}
---
+++ src/Varinit/Main.java
@@ -0,0 +1,22 @@
+package Varinit;
+
+/**
+ * 현재 클래스는 변수 초기화에 대한 클래스입니다.
+ * */
+
+public class Main {
+
+    /**
+     * 간단하게 변수 초기화 하기
+     * */
+    public static void main(String[] args){
+        int year = 2045;
+        int age = 25;
+
+        year = year - age;
+        age = age + 1;
+
+        System.out.println(year);
+        System.out.println(age);
+    }
+}
