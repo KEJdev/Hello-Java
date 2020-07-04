820a86dd118d05de0381fcb9d761154cdc8c9cdc
KEJdev <kej12516@gmail.com>
2020-07-05 00:10:43 +0900

    void 쓰는 이유

--- src/Example/scanner.java
+++ src/Example/scanner.java
@@ -11,7 +11,22 @@ public class scanner {
 
         System.out.println("입력내용 : "+ input); // 엔터가 포함된 출력 
         System.out.print("num=%d%n" + num); // 엔터를 치지 않는 출력
-        System.out.printf("num=%d%n" , num);  // C언어 방식의 함수 
+        System.out.printf("num=%d%n" , num);  // C언어 방식의 함수
 
+    }
+    /**
+     * void를 왜쓰는지?
+     * */
+    private void printStudent(boolean isStudent){
+        if(!isStudent){
+            System.out.println("난 학생이 아닙니다.");
+            return;
+        }
+
+        Scanner scan = new Scanner(System.in);
+        int age = scan.nextInt();
+
+        System.out.println("난 학생입니다.");
+        System.out.println("난" + age + "살 입니다.");
     }
 }
